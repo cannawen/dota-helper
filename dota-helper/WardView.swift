3570e4eb8d6a579b6a1bb93cd3f7cce0f9cc9fc5
Canna Wen <cannawen@gmail.com>
2016-11-26 14:30:31 -0500

    Added countdown for wards [#134980061]

--- dota-helper/WardView.swift
+++ dota-helper/WardView.swift
@@ -12,8 +12,23 @@ class WardView: UIView {
 
     @IBOutlet weak var countdownLabel: UILabel!
     
+    var secondsRemaining = 6 * 60;
+    var timer : Timer!;
+    
     static func loadFromNib() -> WardView {
         return Bundle.main.loadNibNamed("WardView", owner: self, options: nil)![0] as! WardView
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    func updateCounter() {
+        secondsRemaining -= 1;
+        let seconds = secondsRemaining % 60;
+        let minutes = secondsRemaining / 60;
+        countdownLabel.text = "\(minutes):\(seconds)"
     }
 
 }
