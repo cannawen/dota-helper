85435265f489579f8cdef52428bf7fd32e1843e5
Canna Wen <cannawen@gmail.com>
2016-11-26 16:32:58 -0500

    Adding info about wards into WardType enum [#134980079]

--- dota-helper/ImageName.swift
+++ dota-helper/ImageName.swift
@@ -10,4 +10,5 @@ import Foundation
 
 enum ImageName: String {
     case observerWard = "observer_ward"
+    case sentryWard = "sentry_ward"
 }
--- dota-helper/WardType.swift
+++ dota-helper/WardType.swift
@@ -11,4 +11,28 @@ import Foundation
 enum WardType {
     case observer
     case sentry
+    
+    func lifespan() -> TimeInterval {
+        switch self {
+        case .observer:
+            return 6.minutes()
+        case .sentry:
+            return 4.minutes()
+        }
+    }
+    
+    func imageName() -> ImageName {
+        switch self {
+        case .observer:
+            return .observerWard
+        case .sentry:
+            return .sentryWard
+        }
+    }
+}
+
+extension TimeInterval {
+    func minutes() -> TimeInterval {
+        return self * 60
+    }
 }
