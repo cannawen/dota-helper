11e77044e5e9bc9ae9f0959631351f81216ae4a5
Canna Wen <cannawen@gmail.com>
2016-11-26 13:14:35 -0500

    Show minimap image upon app launch [finish #134980049]

--- dota-helper/MapViewController.swift
+++ dota-helper/MapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  dota-helper
 //
 //  Created by Canna Wen on 2016-11-26.
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
